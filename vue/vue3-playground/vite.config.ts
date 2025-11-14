3e14ffcb1240a39d716b29b615bce4091feb4709
kana <orina.kubo@gmail.com>
2025-11-14 11:35:13 +0800

    vue vite构建兼容模式

--- vue/vue3-playground/vite.config.ts
+++ vue/vue3-playground/vite.config.ts
@@ -1,18 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    legacy({
+      targets: ['ios >= 9', 'android >= 4.2', '> 1%'],
+    }),
     vue(),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
